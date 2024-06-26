e7bd1e20f701a76a195e67682d9ae554a4cbafb4
Orbittal123 <165017941+Orbittal123@users.noreply.github.com>
2024-06-26 10:24:43 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {  CountAll  } from "../controllers/admincontroller.js";
+import {  WeekMonthDiff  } from "../controllers/admincontroller.js";
 import {  selectAll  } from "../controllers/selectmodule.js";
 import {  comparebarcode  } from "../controllers/comparebarcode.js";
 import {  insertmoduledata  } from "../controllers/insertmoduledata.js";
@@ -11,6 +12,7 @@ import {  insertmoduledata  } from "../controllers/insertmoduledata.js";
 const router = express.Router();
 
 router.get("/CountAll", CountAll);
+router.get("/WeekMonthDiff", WeekMonthDiff);
 router.get("/selectAll", selectAll);
 router.get("/comparebarcode", comparebarcode);
 router.get("/insertmoduledata", insertmoduledata);
